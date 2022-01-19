Fixed sign error, addded more plotting functionality

python/coupler.py
@@ -8,6 +8,7 @@ from matplotlib.animation import FFMpegWriter
 from matplotlib.animation import PillowWriter
 
 from piston import Piston
+from piston import interpolate
 from movingmesh import Mesh
 from movingmesh import Fluid
 from movingmesh import step
@@ -15,8 +16,8 @@ from movingmesh import step
 t0=0
 tol=10**-5
 maxits=10
-#Mesh size in lfuid
-M=100
+#Mesh size in fluid
+M=20
 #Final time
 T=5.
 #Mesh of fluid
@@ -35,6 +36,7 @@ it=0
 d_prev=0
 p_prev=p=0
 errvec=np.zeros(maxits)
+tpf=[None]*maxits
 timepiston=0
 timefluid=0
 
@@ -44,7 +46,7 @@ while err>tol/5 and it<maxits:
     timepiston+=time.time()-t0
     d_prev=tdva[1,-1]
     tdva=piston.output()
-    # tdva=-tdva
+    tdva[1:,:]=-tdva[1:,:]
     tdva[1,:]+=1
     mesh=Mesh(0., tdva[1,0], M)
     fluid=Fluid(mesh, tdva)
@@ -56,12 +58,34 @@ while err>tol/5 and it<maxits:
     piston=Piston(np.array([0., 0.]), tp)
     
     p_prev=p
+    tpf[it]=tp
     p=tp[1,-1]
-    err=abs(p-p_prev)
-    errvec[it]=err
-    print(err)
+    if it>0:
+        err=abs(p-p_prev)
+        errvec[it-1]=err
+        print(err)
     it+=1
-errvec=errvec[:it]
+    
+
+errtvec=[None]*maxits
+errvec=errvec[:it-1]
+errtvec=errtvec[:it-1]
+tpf=tpf[:it]
+t=tpf[it-1][0,:]
+for i in range(it-1):
+    tp1=tpf[i]
+    tp2=tpf[it-1]
+    errtvec[i]=np.array([interpolate(tp1[0], tp1[1], tp2[0, k])-tp2[1, k] for k in range(len(tp2[0, :]))])
+    #     errtvec[i]=np.array([abs(interpolate(tp1[0], tp1[1], tp2[0, s])-tp2[0, s]) for s in len(range(tp2[0, :]))])
+for errt in errtvec[:6:2]:
+    plt.plot(t, errt)
+plt.yscale("log")
+plt.xlabel(r'$t$')
+plt.ylabel(r'$e_j(t)$')
+plt.legend([r'$e_1(t)$', r'$e_3(t)$', r'$e_5(t)$'], loc='upper left')
+# errtvec=errtvec[:it-1]
+
+
 
 w=fluid.w
 wtot=fluid.wtot
@@ -74,27 +98,35 @@ rhoE=w[:, :, 2]
 
 # p=(gamma-1)*rhoE-0.5*rho*np.abs(u)**2
 
-#plot values at time t
-i=427
-ymin=-4
-ymax=20
-plt.plot(x[i,1:M+1], rho[i, :], 'r-')
-plt.plot(x[i,1:M+1], u[i, :], 'g-')
-plt.plot(x[i,1:M+1], p[i, :], 'b-')
-plt.plot([x[i,-1], x[i, -1]], [ymin, ymax], 'k-')
-tt=t[i]
-plt.text(1.5,15, f't={tt:.2f}', ha="center", va="top")
-plt.xlim(0, 3.2)
-plt.legend([r'$\rho$', r'$\rho u$', r'$\rho E$'], loc='upper left')
-plt.xlabel('t')
+#plot error
+# I=range(len(errvec))
+# plt.plot(I, errvec)
+# plt.yscale("log")
+# plt.xlabel('j')
+# plt.ylabel(r'$e_j^N$')
 
-#Plot total values
+#plot values at time t
+# i=340
+# ymin=-1
+# ymax=5
+# plt.plot(x[i,1:M+1], rho[i, :], 'r-')
+# plt.plot(x[i,1:M+1], u[i, :], 'g-')
+# plt.plot(x[i,1:M+1], p[i, :], 'b-')
+# plt.plot([x[i,-1], x[i, -1]], [ymin, ymax], 'k-')
+# tt=t[i]
+# plt.text(0.6,2, f't={tt:.2f}', ha="center", va="top")
+# plt.xlim(0, 1.2)
+# plt.legend([r'$\rho$', r'$\rho u$', r'$\rho E$'], loc='upper left')
+# plt.xlabel('t')
+
+# Plot total values
 # plt.plot(t, wtot[:,0], 'r-')
 # plt.plot(t, wtot[:,1], 'g-')
 # plt.plot(t, wtot[:,2], 'b-')
 # plt.legend([r'$\rho$', r'$\rho u$', r'$\rho E$'], loc='upper left')
 # plt.xlabel('t')
 
+# Animate
 # fig, ax = plt.subplots()
 # ln0, = plt.plot([], [], 'r-')
 # ln1, = plt.plot([], [], 'g-')
@@ -106,12 +138,12 @@ plt.xlabel('t')
 # fps=60
 # #Time units per second in video
 # tps=12
-# time = plt.text(1.5,15, str(0), ha="left", va="top")
+# time = plt.text(0.6,2, str(0), ha="center", va="top")
 
-# ymin=-4
-# ymax=20
+# ymin=-1
+# ymax=5
 # def init():
-#     ax.set_xlim(0, 3.2)
+#     ax.set_xlim(0, 1.2)
 #     ax.set_ylim(ymin, ymax)
 #     return ln0, ln1, ln2, ln3, 
 

python/piston.py
@@ -38,7 +38,7 @@ class Piston:
         self.p0=50
         self.ms=100
         self.A=0.1
-        self.k=500
+        self.k=5000
     def interpolateinput(self, t):
         return interpolate(self.tp[0], self.tp[1], t)
     def F(self, t):
@@ -54,7 +54,7 @@ class Piston:
         def g(u):
             return u-dt*self.f(u, told+dt)-uold
         def dg(u):
-            return np.eye(np.size(uold))-dt*self.df(u, told+dt)
+            return np.eye(np.size   (uold))-dt*self.df(u, told+dt)
         u= newton(g, dg, uold, 10**-14)
         return u
     def SDIRK12step(self, told, yold, dt):
@@ -62,7 +62,7 @@ class Piston:
         Y1=self.IEstep(told, S1, dt*alpha)
         K1=self.f(Y1, told+dt*alpha)
         S2=yold+dt*(1-alpha)*K1
-        Y2=self.IEstep(told, S2, dt*alpha)
+        Y2=self.IEstep(told+dt*(1-alpha), S2, dt*alpha)
         K2=self.f(Y2, told+dt)
         ynew=S2+dt*alpha*K2
         ytnew=yold+dt*bt1*K1+dt*bt2*K2
@@ -138,19 +138,22 @@ bt1=1-alphat
 bt2=alphat
 def SDIRK2step(f, told, yold, dt):
     S1=yold
-    Y1=IEstep(f, told+dt*alpha, S1, dt*alpha)
-    K1=f(Y1, told+dt*alpha)
+    Y1=IEstep(f, told, S1, dt*alpha)
+    # K1=f(Y1, told+dt*alpha)
+    K1=(Y1-S1)/alpha/dt
     S2=yold+dt*(1-alpha)*K1
-    Y2=IEstep(f, told+dt*alpha, S2, dt*alpha)
+    Y2=IEstep(f, told+dt*(1-alpha), S2, dt*alpha)
     K2=f(Y2, told+dt)
+    # K2=(Y2-S2)/alpha/dt
     ynew=S2+dt*alpha*K2
     return ynew
 def SDIRK12step(f, told, yold, dt):
     S1=yold
     Y1=IEstep(f, told+dt*alpha, S1, dt*alpha)
     K1=f(Y1, told+dt*alpha)
+    # K1=(Y1-S1)/alpha/dt
     S2=yold+dt*(1-alpha)*K1
-    Y2=IEstep(f, told+dt*alpha, S2, dt*alpha)
+    Y2=IEstep(f, told+dt*(1-alpha), S2, dt*alpha)
     K2=f(Y2, told+dt)
     ynew=S2+dt*alpha*K2
     ytnew=yold+dt*bt1*K1+dt*bt2*K2